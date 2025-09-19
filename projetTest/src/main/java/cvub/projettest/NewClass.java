2e95d7b59cf674931ebb7bd0497957589f7f8216
Clément Vazeille <clement.vazeille@gmail.com>
2025-09-19 09:47:22 +0200

    Added a compteur class in the newclass java

--- projetTest/src/main/java/cvub/projettest/NewClass.java
+++ projetTest/src/main/java/cvub/projettest/NewClass.java
@@ -9,5 +9,13 @@ package cvub.projettest;
  * @author utaab
  */
 public class NewClass {
-    
+    static public void compteur(int n)
+    {
+        System.out.println("Debut du compteur");
+        for(int i=0;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("Fin du compteur ^^");
+    }
 }
